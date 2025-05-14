f2808e5b620ad13e7759a653382554c114a93188
Matheus Cartonilho <matheuscartonilho.dev@gmail.com>
2025-05-14 13:23:45 -0400

    Corrige importação circular no init

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
 import os
-from app import app, db
+from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY", "dev-key")
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.secret_key = os.getenv('SECRET_KEY', 'dev-key')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
 with app.app_context():
     db.create_all()
 
-
-from app import routes
+from app import routes 
